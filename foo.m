38fedd3c6e3c6e094e13dca0258395485bf11427
Juan R. Pacha <juan.ramon.pacha@upc.edu>
2021-09-30 07:38:55 +0200

    Chages in foo.m

--- foo.m
+++ foo.m
@@ -34,14 +34,48 @@ fprintf('%16.5e%18.5e\n',vertexsPointP');
 
 %plotElements(nodes, elem, numbering);
 numbering = 0;
-plotElements(nodes, elem, numbering);
+plotElementsOld(nodes, elem, numbering);
 hold on
 plot(p(1,1),p(1,2),'o','Marker','o','MarkerFaceColor','red',...
-    'MarkerSize',10);
+    'MarkerSize',8);
 plot(vertexsPointP(:,1),vertexsPointP(:,2),'o','MarkerFaceColor','green',...
-    'MarkerSize',7)
+    'MarkerSize',6)
 hold off
-    
-    
-    
 
+%Isoparametric function
+r = [0.25, 0.5]; %point on the standard quadrilateral
+
+%Shape functions on the standard quadrilateral R
+Psi1 = @(x,y) (1-x).*(1-y);
+Psi2 = @(x,y) x.*(1-y);
+Psi3 = @(x,y) x.*y;
+Psi4 = @(x,y) (1-x).*y;
+
+%Standard quadrilateral
+vertexsR = [0, 0; 1, 0; 1, 1; 0, 1];
+vertexsRPlot = [vertexsR; vertexsR(1,:)];
+
+%Quadrilateral Q
+vertexsQ = [0,0; 5,-1; 4, 5; 1, 4];
+vertexsQPlot = [vertexsQ; vertexsQ(1,:)];
+
+figure()
+plot(vertexsRPlot(:,1), vertexsRPlot(:,2),'-r')
+hold on
+axis equal
+plot(r(1,1),r(1,2),'o','Marker','o','MarkerFaceColor','blue');
+hold off
+
+alphas = [Psi1(r(1,1),r(1,2)),...
+    Psi2(r(1,1),r(1,2)),...
+    Psi3(r(1,1),r(1,2)),...
+    Psi4(r(1,1),r(1,2))];
+
+p = alphas*vertexsQ; %Point on the quadrilateral Q
+
+figure()
+plot(vertexsQPlot(:,1), vertexsQPlot(:,2),'-b')
+axis equal
+hold on
+plot(p(1,1),p(1,2),'o','Marker','o','MarkerFaceColor','red');
+hold off
